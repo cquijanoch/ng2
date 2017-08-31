9afc167f05a5188890bc1db0dd13ba67505d38de
gary <gary>
2017-08-31 14:53:49 -0500

    barra de desplazamiento modal tabla registroSIAF

--- src/app/modales/listarOrdenes/listarOrdenes.component.ts
+++ src/app/modales/listarOrdenes/listarOrdenes.component.ts
@@ -1,162 +1,175 @@
 import { Component } from '@angular/core';
 import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
 
-import {TableData} from '../../myComponents/tabla/table-data';
+import { TableData } from '../../myComponents/tabla/table-data';
 
 export interface ConfirmModel {
-  title:string;
-  message:string;
+    title: string;
+    message: string;
 }
 
-@Component({  
+@Component({
     selector: 'modal-listarOrdenes',
-    templateUrl: './listarOrdenes.component.html'
+    templateUrl: './listarOrdenes.component.html',
+    styles: [`
+    .modal-dialog{
+        width: 850px;
+        height  : 350px;
+    }
+    .modal-body {
+        width: 850px;
+        height: 350px;
+        overflow-y: auto;
+      }
+  `],
 })
 export class ListarOrdenesComponent extends DialogComponent<ConfirmModel, boolean> implements ConfirmModel {
-  title: string;
-  message: string;
+    title: string;
+    message: string;
 
-  //tabla
-  private data:Array<any> = TableData;
+    //tabla
+    private data: Array<any> = TableData;
 
-  constructor(dialogService: DialogService) {
-    super(dialogService);
-    
-    //informacion de tabla
-    this.length = this.data.length;
-  }
-  confirm() {
-    // we set dialog result as true on click on confirm button, 
-    // then we can get dialog result from caller code 
-    this.result = true;
-    this.close();
-  }
+    public rows: Array<any> = [];
+    public columns: Array<any> = [
+        { title: 'Name', name: 'name', filtering: { filterString: '', placeholder: 'Filter by name' } },
+        {
+            title: 'Position',
+            name: 'position',
+            sort: false,
+            filtering: { filterString: '', placeholder: 'Filter by position' }
+        },
+        { title: 'Office', className: ['office-header', 'text-success'], name: 'office', sort: 'asc' },
+        { title: 'Extn.', name: 'ext', sort: '', filtering: { filterString: '', placeholder: 'Filter by extn.' } },
+        { title: 'Start date', className: 'text-warning', name: 'startDate' },
+        { title: 'Salary ($)', name: 'salary' }
+    ];
+    public page: number = 1;
+    public itemsPerPage: number = 10;
+    public maxSize: number = 5;
+    public numPages: number = 1;
+    public length: number = 0;
 
-// Informacion para la tabla
+    public config: any = {
+        paging: true,
+        sorting: { columns: this.columns },
+        filtering: { filterString: '' },
+        className: ['table-striped', 'table-bordered']
+    };
 
-public rows:Array<any> = [];
-public columns:Array<any> = [
-  {title: 'Name', name: 'name', filtering: {filterString: '', placeholder: 'Filter by name'}},
-  {
-    title: 'Position',
-    name: 'position',
-    sort: false,
-    filtering: {filterString: '', placeholder: 'Filter by position'}
-  },
-  {title: 'Office', className: ['office-header', 'text-success'], name: 'office', sort: 'asc'},
-  {title: 'Extn.', name: 'ext', sort: '', filtering: {filterString: '', placeholder: 'Filter by extn.'}},
-  {title: 'Start date', className: 'text-warning', name: 'startDate'},
-  {title: 'Salary ($)', name: 'salary'}
-];
-public page:number = 1;
-public itemsPerPage:number = 10;
-public maxSize:number = 5;
-public numPages:number = 1;
-public length:number = 0;
+    constructor(dialogService: DialogService) {
+        super(dialogService);
 
-public config:any = {
-  paging: true,
-  sorting: {columns: this.columns},
-  filtering: {filterString: ''},
-  className: ['table-striped', 'table-bordered']
-};
-
-
-
-public ngOnInit():void {
-  this.onChangeTable(this.config);
-}
-
-public changePage(page:any, data:Array<any> = this.data):Array<any> {
-  let start = (page.page - 1) * page.itemsPerPage;
-  let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-  return data.slice(start, end);
-}
-
-public changeSort(data:any, config:any):any {
-  if (!config.sorting) {
-    return data;
-  }
-
-  let columns = this.config.sorting.columns || [];
-  let columnName:string = void 0;
-  let sort:string = void 0;
-
-  for (let i = 0; i < columns.length; i++) {
-    if (columns[i].sort !== '' && columns[i].sort !== false) {
-      columnName = columns[i].name;
-      sort = columns[i].sort;
+        //informacion de tabla
+        this.length = this.data.length;
     }
-  }
-
-  if (!columnName) {
-    return data;
-  }
-
-  // simple sorting
-  return data.sort((previous:any, current:any) => {
-    if (previous[columnName] > current[columnName]) {
-      return sort === 'desc' ? -1 : 1;
-    } else if (previous[columnName] < current[columnName]) {
-      return sort === 'asc' ? -1 : 1;
+    confirm() {
+        // we set dialog result as true on click on confirm button, 
+        // then we can get dialog result from caller code 
+        this.result = true;
+        this.close();
     }
-    return 0;
-  });
-}
 
-public changeFilter(data:any, config:any):any {
-  let filteredData:Array<any> = data;
-  this.columns.forEach((column:any) => {
-    if (column.filtering) {
-      filteredData = filteredData.filter((item:any) => {
-        return item[column.name].match(column.filtering.filterString);
-      });
+    // Informacion para la tabla
+
+
+
+
+
+    public ngOnInit(): void {
+        this.onChangeTable(this.config);
     }
-  });
 
-  if (!config.filtering) {
-    return filteredData;
-  }
-
-  if (config.filtering.columnName) {
-    return filteredData.filter((item:any) =>
-      item[config.filtering.columnName].match(this.config.filtering.filterString));
-  }
-
-  let tempArray:Array<any> = [];
-  filteredData.forEach((item:any) => {
-    let flag = false;
-    this.columns.forEach((column:any) => {
-      if (item[column.name].toString().match(this.config.filtering.filterString)) {
-        flag = true;
-      }
-    });
-    if (flag) {
-      tempArray.push(item);
+    public changePage(page: any, data: Array<any> = this.data): Array<any> {
+        let start = (page.page - 1) * page.itemsPerPage;
+        let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
+        return data.slice(start, end);
     }
-  });
-  filteredData = tempArray;
 
-  return filteredData;
-}
+    public changeSort(data: any, config: any): any {
+        if (!config.sorting) {
+            return data;
+        }
 
-public onChangeTable(config:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
-  if (config.filtering) {
-    Object.assign(this.config.filtering, config.filtering);
-  }
+        let columns = this.config.sorting.columns || [];
+        let columnName: string = void 0;
+        let sort: string = void 0;
 
-  if (config.sorting) {
-    Object.assign(this.config.sorting, config.sorting);
-  }
+        for (let i = 0; i < columns.length; i++) {
+            if (columns[i].sort !== '' && columns[i].sort !== false) {
+                columnName = columns[i].name;
+                sort = columns[i].sort;
+            }
+        }
 
-  let filteredData = this.changeFilter(this.data, this.config);
-  let sortedData = this.changeSort(filteredData, this.config);
-  this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-  this.length = sortedData.length;
-}
+        if (!columnName) {
+            return data;
+        }
 
-public onCellClick(data: any): any {
-  console.log(data);
-}
+        // simple sorting
+        return data.sort((previous: any, current: any) => {
+            if (previous[columnName] > current[columnName]) {
+                return sort === 'desc' ? -1 : 1;
+            } else if (previous[columnName] < current[columnName]) {
+                return sort === 'asc' ? -1 : 1;
+            }
+            return 0;
+        });
+    }
+
+    public changeFilter(data: any, config: any): any {
+        let filteredData: Array<any> = data;
+        this.columns.forEach((column: any) => {
+            if (column.filtering) {
+                filteredData = filteredData.filter((item: any) => {
+                    return item[column.name].match(column.filtering.filterString);
+                });
+            }
+        });
+
+        if (!config.filtering) {
+            return filteredData;
+        }
+
+        if (config.filtering.columnName) {
+            return filteredData.filter((item: any) =>
+                item[config.filtering.columnName].match(this.config.filtering.filterString));
+        }
+
+        let tempArray: Array<any> = [];
+        filteredData.forEach((item: any) => {
+            let flag = false;
+            this.columns.forEach((column: any) => {
+                if (item[column.name].toString().match(this.config.filtering.filterString)) {
+                    flag = true;
+                }
+            });
+            if (flag) {
+                tempArray.push(item);
+            }
+        });
+        filteredData = tempArray;
+
+        return filteredData;
+    }
+
+    public onChangeTable(config: any, page: any = { page: this.page, itemsPerPage: this.itemsPerPage }): any {
+        if (config.filtering) {
+            Object.assign(this.config.filtering, config.filtering);
+        }
+
+        if (config.sorting) {
+            Object.assign(this.config.sorting, config.sorting);
+        }
+
+        let filteredData = this.changeFilter(this.data, this.config);
+        let sortedData = this.changeSort(filteredData, this.config);
+        this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
+        this.length = sortedData.length;
+    }
+
+    public onCellClick(data: any): any {
+        console.log(data);
+    }
 
 }
